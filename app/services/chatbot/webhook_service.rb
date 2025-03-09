91ab2a77675fe3295fd0ccff694a8868d528d123
Dario Chuquilla <at7994267@gmail.com>
2025-03-09 18:31:53 -0500

    Refactor webhook service to consolidate notification logic for success and duplicated requests

--- app/services/chatbot/webhook_service.rb
+++ app/services/chatbot/webhook_service.rb
@@ -11,28 +11,25 @@ module Chatbot
     end
 
     def notify_request_success
-      return if Rails.env.test?
-
-      uri = URI(Rails.application.credentials.dig(:chatbot_url) + "/webhook/requests/notify")
-      request_url = Rails.application.credentials.dig(:api_url) + url
-
-      Net::HTTP.post_form(
-        uri,
-        "userId" => request.user_phone,
-        "message" => I18n.t("requests.new.success", request_url: request_url)
-      )
+      notify_request("requests.new.success")
     end
 
     def notify_request_duplicated
+      notify_request("requests.new.duplicated")
+    end
+
+    private
+
+    def notify_request(message_key)
       return if Rails.env.test?
 
       uri = URI(Rails.application.credentials.dig(:chatbot_url) + "/webhook/requests/notify")
       request_url = Rails.application.credentials.dig(:api_url) + url
 
       Net::HTTP.post_form(
-        uri,
-        "userId" => request.user_phone,
-        "message" => I18n.t("requests.new.duplicated", request_url: request_url)
+      uri,
+      "userId" => request.user_phone,
+      "message" => I18n.t(message_key, request_url: request_url)
       )
     end
   end
