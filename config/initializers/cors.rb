61fa473946965bf56b8b5323d41391f678e4b4f6
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-03-14 09:52:29 -0500

    Allow dev origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001"
+    origins ["localhost:3001","localhost:5173", "dev-web.quientiene.com", "dev-api.quientiene.com"]
 
     resource "*",
       headers: :any,
