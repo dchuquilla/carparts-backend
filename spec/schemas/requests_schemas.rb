f05f6805a2fedf87766edb9a6031e99030f4d0f6
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-03-17 23:37:13 -0500

    Add show_key field to request schemas and update Swagger documentation, hide sensible information in requests responses

--- spec/schemas/requests_schemas.rb
+++ spec/schemas/requests_schemas.rb
@@ -37,15 +37,12 @@ module Schemas
           request: {
             type: :object,
             properties: {
-              id: { type: :integer },
-              user_phone: { type: :string },
-              user_name: { type: :string },
-              user_email: { type: :string },
               part_name: { type: :string },
               part_brand: { type: :string },
               part_model: { type: :string },
               part_year: { type: :integer },
-              part_image: { type: :string }
+              part_image: { type: :string },
+              show_key: { type: :string }
             }
           }
         }
