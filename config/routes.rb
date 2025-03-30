b962c7362bb8d80372bfbf109176c1bdcf8d30a8
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-03-30 03:16:56 -0500

    Implement custom user registration and session controllers for JSON responses; update middleware and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   root to: "rails/health#show"
 
   devise_for :users, controllers: {
-    sessions: "users/sessions"
+    sessions: "users/sessions",
+    registrations: 'users/registrations'
   }
 
   mount Rswag::Ui::Engine => "/api-docs"
