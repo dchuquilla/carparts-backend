7ffcb8dcfdfc53853c9326cc2be09e3808976d78
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-11-12 01:19:37 -0500

    Refactor S3 client initialization into store_image method and remove from constants initializer

--- app/controllers/concerns/storable.rb
+++ app/controllers/concerns/storable.rb
@@ -5,6 +5,13 @@ module Storable
 
   included do
     def store_image(image)
+      S3_CLIENT = Aws::S3::Resource.new(
+        access_key_id: Rails.application.credentials.dig(:r2_access_key),
+        secret_access_key: Rails.application.credentials.dig(:r2_secret_access_key),
+        endpoint: Rails.application.credentials.dig(:r2_endpoint),
+        region: 'auto'
+      )
+
       bucket = S3_CLIENT.bucket(Rails.application.credentials.dig(:r2_bucket))
       obj_id = "#{SecureRandom.uuid}_#{image.original_filename}.webp"
       obj = bucket.object("#{Rails.application.credentials.dig(:r2_environment)}/photos/#{obj_id}")
