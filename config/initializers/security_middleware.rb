9e5f9507a55e5c998d7b3e3065a6dbd3b967737f
Dario Chuquilla <dario.chuquilla@gmail.com>
2026-07-27 15:09:25 -0500

    Add security middleware for headers and rate limiting

--- config/initializers/security_middleware.rb
+++ config/initializers/security_middleware.rb
@@ -1,2 +1,5 @@
+require_relative '../../app/middleware/security_headers_middleware'
+require_relative '../../app/middleware/rate_limit_middleware'
+
 Rails.application.config.middleware.insert_before 0, SecurityHeadersMiddleware
 Rails.application.config.middleware.insert_before 1, RateLimitMiddleware
